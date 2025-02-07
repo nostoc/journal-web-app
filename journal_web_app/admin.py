1143d2483a9b793f6fdd5fbf2a2142465250fed7
nostoc <nostochk@gmail.com>
2025-02-07 20:36:10 +0530

    learning django shell

--- journal_web_app/admin.py
+++ journal_web_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Topic,Entry
+from .models import Topic, Entry
 
 # Register your models here.
 admin.site.register(Topic)
+# model for Entry
 admin.site.register(Entry)
